1ff0ff6d5a321d3bd38a2e047c90bdb7080c93b4
Ahmed Elghareeb <me@rancho.me>
2023-12-17 23:06:33 +0100

    feat: Makes page title dynamic

--- layouts/default.vue
+++ layouts/default.vue
@@ -88,7 +88,7 @@
 
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900">{{ pageName }}</h2>
       </div>
     </header>
     <main>
@@ -103,6 +103,13 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 const route = useRoute()
+
+const pageName = ref(
+  route.name === 'index' ?
+  'Home' : route.name === 'verify' ?
+  'Verify an existing credential' :
+  'Issue a new credential'
+)
 
 const user = {
   name: 'Tom Cook',
